abf22a95d29dd25177bd8444a23d31b11d104e60
MuseGt <nemanja.stefanovic3105@gmail.com>
2023-02-27 19:37:16 +0100

    added routing and navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import CategoiesContainer from "./components/category-container/category-container"
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./routes/home/home.component"
+import Navigation from "./routes/navigation/navigation.component"
 
 const App = () => {
+
   return (
-    <CategoiesContainer />
-  )
+    <Routes>
+      <Route path="/" element={ <Navigation /> }>
+        <Route index element={ <Home /> } />
+      </Route>
+    </Routes>
+  ) 
 }
 
 export default App
